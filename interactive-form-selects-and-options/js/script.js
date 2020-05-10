bf2bcbb6823e553d88cd77c8fe16b0e2b96b2065
browe83 <rowe.brandon.j@gmail.com>
2020-05-10 18:50:47 -0400

    final

--- interactive-form-selects-and-options/js/script.js
+++ interactive-form-selects-and-options/js/script.js
@@ -18,17 +18,22 @@
   // 1a) Target the "Select a shirt" `option` element with a querySelector, like so — ('option[value="Select a shirt"]') 
   // 1b) Use dot notation to set the `hidden` property to true on the "Select a shirt" `option` element 
 
-
+const selectAshirt = document.querySelector('option[value="Select a shirt"]');
+selectAshirt.hidden = true; 
 // 2) Follow the steps below to practice manually selecting which option will show in the "Shirt Selection" field
   const shirtSelectElement = document.querySelector('#shirt-select-element');
   const shirtOptionElements = document.querySelectorAll('#shirt-select-element option');
 
   // 2a) Log both variables to the console to confirm their values
-  // 2b) Use the second variable and bracket notation to select one of the option elements and try logging it out to confirm its value 
+  //console.log(shirtSelectElement);
+  //console.log(shirtOptionElements);
+  // 2b) Use the second variable and bracket notation to select one of the option elements and try logging it out to confirm its value
+  //console.log(shirtOptionElements[1]);
   // 2c) Use dot notation to set the `selected` property to true on the `option` element you selected above
+//shirtOptionElements[1].selected = false; 
   // 2d) Refresh the browser to see the new value in the `select` element
   // 2e) Experiment with setting the `selected` property to true or false on different option elements to see the results
-
+//shirtOptionElements[2].selected = false; 
 
 /**
  * Event listener for checkboxes
@@ -37,11 +42,24 @@ shirtSelectElement.addEventListener('change', (event) => {
 
 // 3) Follow the steps below to highlight the selected shirt 
   // 3a) Log out the value of the `select` element with `event.target` technique — console.log(event.target.value);
+  //console.log(event.target.value);
   // 3b) Create a variable to store the shirt `img` elements, using a querySelectorAll, like so — ('.shirt-image') 
+  const shirtImages = document.querySelectorAll('.shirt-image');
   // 3c) Create a `for` loop to iterate over the shirt `img` elements in the variable above
   // 3d) Inside the loop, create the following two variables:
     // One to store the alt attribute of the image at the loop's current iteration, like so: YourImgVariableName[i].alt
     // One for the `event.target.value`
+    for (let i = 0; i < shirtImages.length; i++) {
+      const altAttribute = shirtImages[i].alt; 
+      const eventTarget = event.target.value; 
+        if (altAttribute === eventTarget) {
+              shirtImages[i].classList.add('chosen'); 
+          } else { shirtImages[i].classList.remove('chosen')
+        
+          }
+        // console.log(altAttribute); 
+        // console.log(eventTarget); 
+      }
   // 3e) Log out the two variables, refresh the page, and select a new shirt to confirm their values
   // 3f) Still inside the loop, create an if/else statement 
   // 3g) If the two variables you just created are equal, use the `.classList.add()` method to add the 'chosen' className to the img element
